96a554c332c6878b45d1ea52d0d601a3b787d7bf
Nate Finch <nate.finch@gmail.com>
2013-01-16 13:42:01 -0500

    add command documentation

---
+++ doc.go
@@ -0,0 +1,20 @@
+/*
+
+Command gocog creates an executable that will generate text from sourcecode inlined in another file.
+
+Usage:
+	gocog [OPTIONS] [INFILE1] [@FILELIST] ...
+
+	Runs gocog over each infile.
+	Filenames prepended with @ are assumed to be newline delimited lists of files to be processed.
+
+Help Options:
+	-h, --help    Show this help message
+
+Application Options:
+	-z        The [[[end]]] marker can be omitted, and is assumed at eof.
+	-v        toggles verbose output (overridden by -q)
+	-q        turns off all output
+	-S        Write to the specified cog files serially (default is parallel)
+*/
+package documentation
